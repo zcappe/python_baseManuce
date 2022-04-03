40278b86fb4e7c1faa5476d5c10e9511a63b590e
zcappe <zoecappe@orange.fr>
2022-04-03 20:03:39 +0200

    le site fonctionne avec les utilisateuuuuuuuuuuuuuuurs

--- base_manuce/basemanuce/routes/__init__.py
+++ base_manuce/basemanuce/routes/__init__.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect
 
 
 from ..app import app, login
@@ -69,18 +69,55 @@ def recherche_simple():
                            titre=titre)
 
 
-@app.route("/inscription")
+@app.route("/inscription", methods=["GET", "POST"])
 def inscription():
-    return render_template("pages/inscription.html", nom="Base Manuce")
+    """Cette route permet de s'inscrire avec le formulaire de la page"""
+    if request.method == "POST":
+        statut, donnees = User.creer(
+            email=request.form.get("email", None),
+            pseudo=request.form.get("pseudo", None),
+            nom=request.form.get("nom", None),
+            motdepasse=request.form.get("motdepasse", None)
+        )
+        if statut is True:
+            flash("Votre inscription a bien été prise en compte. Vous pouvez maintenant vous identifier.",
+                  "success")
+            return redirect("/")
+        else:
+            flash("Les erreurs suivantes ont été rencontrées : " + ",".join(donnees), "error")
+            return render_template("pages/inscription.html")
+    else:
+        return render_template("pages/inscription.html", nom="Base Manuce")
 
 
-@app.route("/connexion")
+@app.route("/connexion", methods=["POST", "GET"])
 def connexion():
+    """Cette route permet de se connecter avec le formulaire de la page"""
+    if current_user.is_authenticated is True:
+        flash("Vous êtes déjà connecté", "info")
+        return redirect("/")
+    if request.method == "POST":
+        utilisateur = User.identification(
+            pseudo=request.form.get("pseudo", None),
+            motdepasse=request.form.get("motdepasse", None)
+        )
+        if utilisateur:
+            flash("Vous êtes maintenant connecté", "success")
+            login_user(utilisateur)
+            return redirect("/")
+        else:
+            flash("Vos identifiants sont erronés ou vous n'êtes pas encore inscrit.", "error")
+
     return render_template("pages/connexion.html", nom="Base Manuce")
+login.login_view = 'connexion'
 
 
 @app.route("/deconnexion")
 def deconnexion():
+    """Cette route permet de se déconnecter"""
+    if current_user.is_authenticated is True:
+        logout_user()
+    flash("Vous êtes déconnecté", "info")
     return render_template("pages/deconnexion.html", nom="Base Manuce")
 
 
