411dcfe0557aa3633cdf526f5df044a53e710bdc
zcappe <zoecappe@orange.fr>
2022-04-30 17:54:39 +0200

    modifs + début form

--- base_manuce/basemanuce/modeles/donnees.py
+++ base_manuce/basemanuce/modeles/donnees.py
@@ -4,24 +4,6 @@ from ..app import db
 # Avec les class ...(db.Model), on crée ce qui correspond aux tables dans la base de données,
 # pour ensuite y insérer des objets (enregistrements)
 # On a ici les trois tables contenant les imprimeurs, les institutions de conservations et les livres
-class Printers(db.Model):
-    __tablename__ = 'printers'
-    printer_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
-    printer_name = db.Column(db.Text, nullable=False)
-    birthyear = db.Column(db.Integer)
-    deathyear = db.Column(db.Integer)
-    printer_othername1 = db.Column(db.Text)
-    printer_othername2 = db.Column(db.Text)
-    description = db.Column(db.Text)
-
-
-class Institutions(db.Model):
-    __tablename__ = 'institutions'
-    institution_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
-    country = db.Column(db.Text, nullable=False)
-    city = db.Column(db.Text, nullable=False)
-    institution_name = db.Column(db.Text, nullable=False)
-
 
 class Books(db.Model):
     __tablename__ = 'books'
@@ -33,3 +15,27 @@ class Books(db.Model):
     identifier = db.Column(db.Text, nullable=False)
     id_printer = db.Column(db.Integer, db.ForeignKey('printers.printer_id'))
     id_institution = db.Column(db.Integer, db.ForeignKey('institutions.institution_id'))
+    printer = db.relationship("Printers", back_populates="book", foreign_keys=id_printer)
+    institution = db.relationship("Institutions", back_populates="book", foreign_keys=id_institution)
+
+
+class Printers(db.Model):
+    __tablename__ = 'printers'
+    printer_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
+    printer_name = db.Column(db.Text, nullable=False)
+    birthyear = db.Column(db.Integer)
+    deathyear = db.Column(db.Integer)
+    printer_othername1 = db.Column(db.Text)
+    printer_othername2 = db.Column(db.Text)
+    description = db.Column(db.Text)
+    book = db.relationship("Books", back_populates="printer", foreign_keys=Books.id_printer)
+
+
+class Institutions(db.Model):
+    __tablename__ = 'institutions'
+    institution_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
+    country = db.Column(db.Text, nullable=False)
+    city = db.Column(db.Text, nullable=False)
+    institution_name = db.Column(db.Text, nullable=False)
+    book = db.relationship("Books", back_populates="institution", foreign_keys=Books.id_institution)
+
--- base_manuce/basemanuce/routes/routes_form.py
+++ base_manuce/basemanuce/routes/routes_form.py
@@ -1,11 +1,22 @@
 from flask import render_template, request
+from flask_login import login_required
 
 
 from ..app import app
-from ..modeles.donnees import Books, Printers
-from ..constantes import LIVRES_PAR_PAGE
+from ..modeles.donnees import Books, Printers, Institutions
 
 
-@app.route("/formulaire")
-def formulaire():
+@app.route("/book/<int:book_id>/form", methods=["GET", "POST"])
+@login_required
+def formulaire(book_id):
+    mon_livre = Books.query.get_or_404(book_id)
+    imprimeurs = Printers.query.all()
+    institutions = Institutions.query.all()
+
+    erreurs = []
+    updated = False
+
+    if request.method == "POST":
+        if not request.form.get("title")
+
     return render_template("pages/formulaire.html", nom="Base Manuce")
